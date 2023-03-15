fb5cfe21caf4e96d1d4a819d16a558ca2dc49ce9
faranbutt <faranbutt789@gmail.com>
2023-03-15 23:34:10 +0500

    Updating

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero (){
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, Im <span className='text-[#915eff]'>Faran</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi I am <span className='text-[#915eff]'>Faran</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white`}>I develop Full Stack Applications,<br className='sm:block hidden'/> AI products and Devops</p>
         </div>
       </div>
