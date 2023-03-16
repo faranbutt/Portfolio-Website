223c2b1ac0e267205763a641464f68f8b1ed11ea
faranbutt <faranbutt789@gmail.com>
2023-03-16 20:03:25 +0500

    Updating

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -1,7 +1,22 @@
+import {VerticalTimeline,VerticalTimelineElement} from 'react-vertical-timeline-component';
+import {motion} from 'framer-motion';
+import { textVariant } from '../utils/motion';
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
 
-export default function Experience() {
+const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>
+        What I have done so far
+      </p>
+      <h2 className={styles.sectionHeadText}>
+        Work Experience
+      </h2>
+    </motion.div>
+    </>
   )
 }
 
+export default SectionWrapper(Experience,'work');
