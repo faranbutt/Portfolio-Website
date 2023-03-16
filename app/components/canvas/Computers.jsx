cdd11de4a08c10f09491737e883982ecb1046672
faranbutt <faranbutt789@gmail.com>
2023-03-16 13:31:05 +0500

    Updating

--- app/components/canvas/Computers.jsx
+++ app/components/canvas/Computers.jsx
@@ -31,13 +31,10 @@ export default function ComputerCanvas(){
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
