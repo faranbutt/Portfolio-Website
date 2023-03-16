1cd26657e98048992c17efc86c51f6ddd415617c
faranbutt <faranbutt789@gmail.com>
2023-03-16 16:48:18 +0500

    Updating

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,32 +4,34 @@ import {motion} from 'framer-motion';
 import {styles} from '../styles';
 import {services} from '../constants/index'
 import {fadeIn, textVariant} from '../utils/motion';
-import Tilt from "react-tilt";
 import Image from 'next/image';
-const ServiceCard = ({index,title,icon}) => {
+import VanillaTilt from 'vanilla-tilt';
+
+const ServiceCard = ({index,title,icon,options}) => {
+
   return(
-    <Tilt className="xs:w-[250px] w-full">
+    <div className="xs:w-[250px] w-full">
       <motion.div
       variants={fadeIn('right',"spring",0.5*index,0.45)}
       className={"w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"}
       >
-        <div options={{
-          max:45,
-          scale:1,
-          speed:450
-        }}
+        <div options={options}
          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
          >
           <Image src={icon} alt={title} className="w-16 h-16 object-contain" />
           <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
         </div>
       </motion.div>
-
-    </Tilt>
+      </div>
   );
 }
 
 export default function About(){
+  const options = {
+    max:45,
+    scale:1,
+    speed:450
+  }
   return (
     <>
     <motion.div variants={textVariant()} className={'mt-20'} >
@@ -50,7 +52,7 @@ export default function About(){
     </motion.p>
     <div className='mt-20 flex flex-wrap gap-10'>
       {services.map((service,index)=>(
-        <ServiceCard key={service.title} index={index} {...service}/>
+        <ServiceCard key={service.title} index={index} options={options} {...service}/>
       ))}
     </div>
     </>
