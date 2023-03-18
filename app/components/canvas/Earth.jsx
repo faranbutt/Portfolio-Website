7770a6f96959b1b7801cece97a17fac735e3e587
faranbutt <faranbutt789@gmail.com>
2023-03-18 19:33:14 +0500

    Pushing

--- app/components/canvas/Earth.jsx
+++ app/components/canvas/Earth.jsx
@@ -1,6 +1,28 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+const Earth = () => {
+  return(
+    <div>
+      Earth
+    </div>
+  );
+}
 
 export default function EarthCanvas(){
   return (
-    <div>Earth</div>
+    <Canvas shadows frameloop="demand" gl={{preserveDrawingBuffer: true}} camera={{}}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+        autoRotate
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+        /> 
+        <Earth />
+      </Suspense>
+    </Canvas>
   )
 }
