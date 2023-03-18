006f443a1b3e2b031bb44711b1593025e3d303e6
faranbutt <faranbutt789@gmail.com>
2023-03-18 18:52:23 +0500

    Pushing

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -22,7 +22,28 @@ const Contact = () => {
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
-
+      <motion.div variants={slideIn('left','tween',0.2,1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
+        <p className={styles.sectionSubText}>
+          Get in Touch
+        </p>
+        <h3 className={styles.sectionHeadText}>
+          Contact.
+        </h3>
+        <form action="" ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
+          <label htmlFor="" className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input type="text" name="name" value={form.name} onChange={handleChange} placeholder={'Whats your name?'} className={"bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"} />
+          </label>
+          <label htmlFor="" className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input type="email" name="email" value={form.email} onChange={handleChange} placeholder={'Whats your email?'} className={"bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"} />
+          </label>
+          <label htmlFor="" className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <input rows="7" name="message" value={form.message} onChange={handleChange} placeholder={'What do  your want to say?'} className={"bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"} />
+          </label>
+        </form>
+      </motion.div>      
     </div>
     
   )
