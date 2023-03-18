ab3bece11eb70fec2ec488e789557cc883fcaf09
faranbutt <faranbutt789@gmail.com>
2023-03-18 20:28:02 +0500

    Pushing

--- app/components/canvas/Earth.jsx
+++ app/components/canvas/Earth.jsx
@@ -7,13 +7,22 @@ import CanvasLoader from "../Loader";
 const Earth = () => {
   const earth = useGLTF('./planet/scene.gltf');
   return(
-    <primitive object={earth.scene} />
+    <primitive object={earth.scene} 
+    scale={2.5}
+    position-y={0}
+    rotation-y={0}
+    />
   );
 }
 
 export default function EarthCanvas(){
   return (
-    <Canvas shadows frameloop="demand" gl={{preserveDrawingBuffer: true}} camera={{}}>
+    <Canvas shadows frameloop="demand" gl={{preserveDrawingBuffer: true}} camera={{
+      fov:45,
+      near:0.1,
+      far:200,
+      position:[-4,3,6]
+    }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls 
         autoRotate
